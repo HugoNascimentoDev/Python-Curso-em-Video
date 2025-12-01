1c5f75e25a82774ecccafba5b6c88036bc6acec6
Hugo Nascimento <hugoalohmundo@gmail.com>
2025-12-01 20:10:36 -0300

    Create Curso Python #04 - Primeiros comandos em Python3.py

---
+++ MUNDO 01/Curso Python #04 - Primeiros comandos em Python3.py
@@ -0,0 +1,13 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+print(f'Seu nome é {nome} e você tem {idade} anos.')
+
+
+# Desafio 01 - Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado.
+
+
+# Desafio 02 - Crie um scrip Python que leia o ano, dia, o mês e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada.
+
+# Desafio 03 - Crie um script Python que leia dois números e tente mostra a soma entre eles.
+
