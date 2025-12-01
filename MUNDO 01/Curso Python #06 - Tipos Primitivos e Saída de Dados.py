95261710c484c790095d045ffa378bee1464444f
Hugo Nascimento <hugoalohmundo@gmail.com>
2025-12-01 20:10:42 -0300

    Create Curso Python #06 - Tipos Primitivos e Saída de Dados.py

---
+++ MUNDO 01/Curso Python #06 - Tipos Primitivos e Saída de Dados.py
@@ -0,0 +1,17 @@
+n1 = input("Digite o primeiro valor: ")
+n2 = input("Digite o segundo valor: ")
+
+soma = n1 + n2
+
+print(soma)
+
+
+print(type(n1))
+
+n1 = int(input("Digite o primeiro valor: "))
+n2 = int(input("Digite o segundo valor: "))
+
+soma = n1 + n2
+
+print(f'Resultado: \n{n1} + {n2} = {soma}')
+
