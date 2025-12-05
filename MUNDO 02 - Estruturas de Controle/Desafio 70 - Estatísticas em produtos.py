8e0b425c8f8670e247ce9c183a4d4bfd3d31f5e0
Hugo Nascimento <hugoalohmundo@gmail.com>
2025-12-05 00:27:16 -0300

    Finalizando o mundo 02 o/
    Finalizando o mundo 02 o/ - 05.12.2025

--- MUNDO 02 - Estruturas de Controle/Desafio 70 - Estatísticas em produtos.py
+++ MUNDO 02 - Estruturas de Controle/Desafio 70 - Estatísticas em produtos.py
@@ -2,3 +2,39 @@
 # A) qual é o total gasto na compra.
 # B) quantos produtos custam mais de R$1000.
 # C) qual é o nome do produto mais barato. 
+
+
+linha = '=' * 35
+
+print(linha)
+print('\tLOJAS NASCIMENTO')
+print(linha)
+contador = acumulador = mais_mil = preco_mais_barato = 0
+nome_produto_mais_barato = ' '
+
+while True:
+    produto = input('Nome do Produto: ')
+    preco = float(input('Preço: R$ '))
+    print(linha)
+    acumulador += preco
+    contador += 1
+    resp = ' '
+    if preco > 1000: 
+        mais_mil += 1
+    if contador == 1:
+        nome_produto_mais_barato = produto
+        preco_mais_barato = preco
+    else:
+        if preco < preco_mais_barato:
+            nome_produto_mais_barato = produto
+            preco_mais_barato = preco
+    while resp not in 'SN':
+        resp = input('Deseja continuar [S/N]? ').upper().strip()[0]
+        print(linha)
+    if resp == 'N':
+        break
+print(f'\t    RESUMO\n{linha}')
+print(f'Total Gasto R$ {acumulador:.2f}')
+print(f'Total de produtos mais caro que R$ 1000,00, {mais_mil} produtos.')
+print(f'O produto {nome_produto_mais_barato.upper()} é o item mais barato, custando R$ {preco_mais_barato:.2f}\n{linha}\n')
+
--- MUNDO 02 - Estruturas de Controle/Desafio 71 - Simulador de Caixa Eletrônico.py
+++ MUNDO 02 - Estruturas de Controle/Desafio 71 - Simulador de Caixa Eletrônico.py
@@ -1,2 +1,41 @@
 # Exercício Python 071: Crie um programa que simule o funcionamento de um caixa eletrônico. No início, pergunte ao usuário qual será o valor a ser sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues.
 # OBS: considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
+
+linha = '=' * 50
+
+print(linha)
+print('\t\tBANCO NASCIMENTO')
+print(linha)
+valor = 0
+
+valor = int(input('Qual o valor deseja sacar? '))
+print(linha)
+total = valor
+cedula = 100
+contador_cedula = 0
+
+while True:
+    if total >= cedula:
+        total -= cedula
+        contador_cedula += 1
+    else:
+        if contador_cedula > 0:
+            print(f'Total de {contador_cedula} cédulas de R$ {cedula:.2f}')
+        if cedula == 100:
+            cedula = 50
+        elif cedula == 50:
+            cedula = 20
+        elif cedula == 20:
+            cedula = 10
+        elif cedula == 10:
+            cedula = 5
+        elif cedula == 5:
+            cedula = 1
+        contador_cedula = 0
+        if total == 0:
+            break
+
+
+
+print(linha)
+print('Volte sempre ao BANCO NASCIMENTO! Tenha um bom dia!\n\n')
