451b0e0cb5f79ed7da321ac5453a479b2d9ad3ad
Hugo Nascimento <hugoalohmundo@gmail.com>
2026-02-04 23:10:45 -0300

    04.02.2026

--- MUNDO 03 - Estruturas Compostas/Desafio 101 - Funções para votação.py
+++ MUNDO 03 - Estruturas Compostas/Desafio 101 - Funções para votação.py
@@ -3,22 +3,24 @@
 
 # Exercício Python 101: Crie um programa que tenha uma função chamada voto() que vai receber como parâmetro o ano de nascimento de uma pessoa, retornando um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL e OBRIGATÓRIO nas eleições.
 
-from datetime import datetime
+
 
 print('-' * 60)
 ano = int(input('Digite seu ano de nascimento: [yyyy] '))
 
 def voto (ano = 0):
+    from datetime import datetime
     ano_atual = datetime.now().year
     idade = ano_atual - ano
-    if idade >= 70 or idade == 16 or idade == 17:
+    if ano > ano_atual:
+        idade = 'ERROR'
+        status_voto = '<<< VALOR INVÁLIDO! TENTE NOVAMENTE >>>'
+    elif idade >= 70 or idade == 16 or idade == 17:
         status_voto = 'Seu voto é OPCIONAL!'
     elif idade < 16:
         status_voto = 'NÃO VOTA!'
     elif idade >= 18 and idade < 70:
         status_voto = 'Seu voto é OBRIGATÓRIO!'
-    else:
-        status_voto = '<<< VALOR INVÁLIDO! TENTE NOVAMENTE >>>'
         
     return idade, status_voto
 
