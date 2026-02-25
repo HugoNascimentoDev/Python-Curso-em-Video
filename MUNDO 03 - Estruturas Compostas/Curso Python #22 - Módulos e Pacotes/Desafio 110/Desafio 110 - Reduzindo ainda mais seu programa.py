0bcb1a2f73d20fae97cb638291911181375e48f7
Hugo Nascimento <hugoalohmundo@gmail.com>
2026-02-24 22:43:34 -0300

    24.02.2026

---
+++ MUNDO 03 - Estruturas Compostas/Curso Python #22 - Módulos e Pacotes/Desafio 110/Desafio 110 - Reduzindo ainda mais seu programa.py
@@ -0,0 +1,10 @@
+# # Arquivo Desafio 108.py
+# # Seu código aqui
+
+# Exercício Python 110: Adicione ao módulo moeda.py criado nos desafios anteriores uma função chamada resumo(), que mostre na tela algumas informações geradas pelas funções que já temos no módulo criado até aqui.
+import moeda110
+
+n = float(input('Digite um valor: '))
+
+moeda110.resumo(n, 0.10, 0.15)
+
